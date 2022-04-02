58705459574900ba7434965547907e7960850eaf
tareqshawwasoft <tareqsoftwork@gmail.com>
2022-04-03 00:11:20 +0300

    Website stats for admin

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Doctor;
+use App\Models\Patient;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function index()
     {
-
-        // if(Auth::user()->type == 'patient') {
-        //     return redirect('/');
-        // }
-
-        // dd(Auth::user());
-        return view('admin.index');
+        $patients = Patient::count();
+        $doctors=Doctor::count();
+        $admins=User::where('type','admin')->count();
+        return view('admin.index', compact('patients','admins','doctors'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,94 @@
 @extends('admin.master')
 
 @section('content')
+@section('styles')
+    <style>
+        #title {
+            color: black;
+            font-weight: bold;
+        }
+
+        #numbers {
+            font-weight: bold;
+        }
+
+        .grid-container {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+            gap: 20px;
+            margin-top: 80px;
+        }
+
+
+        .container {
+
+            margin-left: auto;
+            margin-right: auto;
+            padding-left: 15px;
+            padding-right: 15px;
+
+        }
+
+        h5 {
+            margin: 12px;
+        }
+
+
+        img {
+            width: 10%;
+            height: 60%
+        }
+
+
+        .card-title {
+            text-align: center;
+        }
+
+        .card-department {
+            text-align: center;
+            font-size: 1rem;
+        }
+
+        .card-email {
+            text-align: center;
+            font-size: 1rem;
+        }
+
+    </style>
+@stop
 <!-- Page Heading -->
-<h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+<h1 class="h3 mb-4 text-center">Website Statistics</h1>
+<div class="container">
+    <div class="grid-container">
+
+
+        <div class="card" style="width: 15rem; height: 8rem;">
+            <div class="card-body">
+                <h5 id="title" class="card-title text-center mb-3">Patients</h5>
+                <div class="container" style="align-content: center;">
+                    <h6 class="d-flex justify-content-center"><span id="numbers">{{$patients}}</span></h6>
+                </div>
+            </div>
+
+        </div>
+        <div class="card" style="width: 15rem; height: 8rem;">
+            <div class="card-body">
+                <h5 id="title" class="card-title text-center mb-3">Doctors</h5>
+                <div class="container" style="align-content: center;">
+                    <h6 class="d-flex justify-content-center"><span id="numbers">{{$doctors}}</span></h6>
+                </div>
+            </div>
+
+        </div>
+        <div class="card" style="width: 15rem; height: 8rem;">
+            <div class="card-body">
+                <h5 id="title" class="card-title text-center mb-3">Admins</h5>
+                <div class="container" style="align-content: center;">
+                    <h6 class="d-flex justify-content-center"><span id="numbers">{{$admins}}</span></h6>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
 @stop
